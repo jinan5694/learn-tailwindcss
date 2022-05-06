bf176aeabfc83e12b79ea53ee331895f4694ca9f
纪南 <jinan5694@163.com>
2022-05-07 00:36:32 +0800

    test & build

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-red-900 font-bold text-4xl font-mono">This is an about page</h1>
+    <p class="text-base text-stone-600">this is a description</p>
+    <div class="shadow-md p-4 w-80 h-40 rounded-lg bg-rose-50 text-sky-900 text-lg">
+      <div class="flex justify-between items-center h-8">
+        <div class="text-stone-900 text-lg">title</div>
+        <div class="text-gray-500">close</div>
+      </div>
+      card
+    </div>
+    <p class="text-sm hover:text-base">hello world</p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
